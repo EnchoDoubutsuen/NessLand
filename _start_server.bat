b3cb2937eb905ca86c14f30df47e9d5e691f1275
Encho.Doubutsuen <encho.doubutsuen@gmail.com>
2018-06-17 23:24:10 +0900

    commit by batch

--- _start_server.bat
+++ _start_server.bat
@@ -1,1 +1,1 @@
-java -server -Xms4G -Xmx4G -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy -Xmn1G -XX:MaxNewSize=1G -jar minecraft_server.1.12.2.jar nogui
+java -server -Xms6G -Xmx6G -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy -Xmn2G -XX:MaxNewSize=2G -jar minecraft_server.1.12.2.jar nogui
